8ea7b6c91c3278a681c11b9b098fd5d9c50a03d6
Guilherme Henrique <115844984+GuilhermeHenrii@users.noreply.github.com>
2023-11-14 13:46:41 -0300

    ex006-soma-de-digitos

---
+++ ex006/somaDosDigitos.js
@@ -0,0 +1,19 @@
+function digitalRoot(n) {
+    let digits = String(n);
+    let numberInArray = String(n).split('');
+    let result = 0;
+
+    while (digits.length >= 2){
+        for(let i = 0; i <= numberInArray.length - 1; i++){
+            let values = Number(numberInArray[i]);
+            result += values;
+            digits = String(result);
+        };
+
+        numberInArray = String(result).split('');
+    };
+
+    return result;
+};
+
+console.log(digitalRoot(942));
